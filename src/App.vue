ea8911678ffaaa0fdc58959fd9cdd2dfabfff7a3
Alexey Motovilov <motowelove@gmail.com>
2020-04-22 19:16:45 +0300

    push

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <div v-for="(text, i) in tips" :key="i">
+      <tip :text-for-tip="text"/>
+      <button @click="showNumber(i)"> show {{ i }}</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import Tip from '@/components/ui/Tip.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Tip,
+  },
+  data() {
+    return {
+      tips: ['1', '2', '3', 4, 5],
+    }
+  },
+  methods: {
+    showNumber(val) {
+      console.log(val);
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  > div {
+  }
+}
+</style>
---
+++ src/components/ui/Tip.vue
@@ -0,0 +1,66 @@
+<template>
+  <span class="tip">
+    <warning-icon />
+    <div class="text">
+      <p>{{ textForTip }}</p>
+
+    </div>
+
+  </span>
+</template>
+
+<script>
+import WarningIcon from '@/components/icons/WarningIcon.vue'
+
+export default {
+  components: {
+    WarningIcon,
+  },
+  props: {
+    textForTip : {
+      required : true,
+      default: 'tip'
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+$mainColor: #4C5056;
+.tip {
+  position: relative;
+  &:hover .text {
+    display: block;
+  }
+  .text {
+    min-width: 200px;
+    display: none;
+    background-color: $mainColor;
+    border-radius: 5px;
+    color: #fff;
+    position: absolute;
+    top: 22px;
+    left: 50%;
+    transform: translate(-50%);
+    &:after {
+      content: "";
+      position: absolute;
+      width: 0;
+      height: 0;
+      border-left: 5px solid transparent;
+      border-right: 5px solid transparent;
+      border-bottom: 5px solid $mainColor;
+      transform: rotate(-180deg);
+      top: -5px;
+      left: 50%;
+      transform: translate(-50%);
+    }
+    p {
+      margin-top: 0;
+      margin-bottom: 0;
+      padding: 8px;
+    }
+
+  }
+}
+</style>
